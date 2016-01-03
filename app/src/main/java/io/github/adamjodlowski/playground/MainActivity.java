eec15204ea676e696ad743350b5893ff31d84f34
Adam Jodłowski <adamjodlowski@users.noreply.github.com>
2016-01-03 12:40:11 +0100

    Create "Getting Started" project

---
+++ app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
@@ -0,0 +1,28 @@
+package io.github.adamjodlowski.playground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("It works!");
+            }
+        });
+    }
+}
