965aeea20393c9e78a5e722454c5d89f531dccd8
Adam Jodłowski <adamjodlowski@users.noreply.github.com>
2016-01-09 20:00:41 +0100

    Add logic code for sending SMS (targetSDK = 22)

--- app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
+++ app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
@@ -2,13 +2,17 @@ package io.github.adamjodlowski.playground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editTextPhone;
+    EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        editTextPhone = (EditText) findViewById(R.id.editTextPhone);
+        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("It works!");
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(
+                        editTextPhone.getText().toString(),
+                        null,
+                        editTextMessage.getText().toString(),
+                        null,
+                        null);
+
+                textView.setText("SMS sent");
             }
         });
     }
