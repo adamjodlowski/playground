a3aff751834c124b8fc02bedc432ce36407e9c0e
Adam Jodłowski <adamjodlowski@users.noreply.github.com>
2016-01-24 14:18:31 +0100

    Create basic example of Observable & Observer

--- app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
+++ app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
@@ -2,27 +2,44 @@ package io.github.adamjodlowski.playground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
 
-    TextView textView;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
+        integerObservable.subscribe(integerObserver);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("It works!");
-            }
-        });
     }
+
+    Observable<Integer> integerObservable = Observable.just(4, 8, 15, 16, 23, 42);
+
+    Observer<Integer> integerObserver = new Observer<Integer>() {
+        @Override
+        public void onCompleted() {
+            Log.d("PLAYGROUND", "onCompleted");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.d("PLAYGROUND", "onError", e);
+        }
+
+        @Override
+        public void onNext(Integer integer) {
+            Log.d("PLAYGROUND", "onNext: " + integer);
+        }
+    };
+
 }
