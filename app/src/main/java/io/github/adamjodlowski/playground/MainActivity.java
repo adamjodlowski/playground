a60869a803de7707c58b03c9936937377c0989b5
Adam Jodłowski <adamjodlowski@users.noreply.github.com>
2016-03-20 20:16:27 +0100

    Add action menu

--- app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
+++ app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
@@ -3,6 +3,10 @@ package io.github.adamjodlowski.playground;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action1:
+                Log.d("PLAYGROUND", "Action 1 selected");
+                return true;
+            case R.id.action2:
+                Log.d("PLAYGROUND", "Action 2 selected");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
