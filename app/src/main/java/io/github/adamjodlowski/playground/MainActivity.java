792afefa626054d6ee6f1672de7c8949377d775e
Adam Jodłowski <adamjodlowski@users.noreply.github.com>
2016-03-20 19:15:11 +0100

    Add and setup empty Toolbar

--- app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
+++ app/src/main/java/io/github/adamjodlowski/playground/MainActivity.java
@@ -1,28 +1,18 @@
 package io.github.adamjodlowski.playground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("It works!");
-            }
-        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+    
 }
